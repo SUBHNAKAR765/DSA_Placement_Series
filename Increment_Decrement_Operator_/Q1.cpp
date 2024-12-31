d41e80769c0a311b9e95a148d66e5dd8390d7485
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2025-01-01 00:26:59 +0530

    Add files via upload

---
+++ Increment_Decrement_Operator_/Q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a= 5;
+
+     cout << a++ <<endl;
+     cout << ++a <<endl;
+     cout << --a <<endl;
+     cout << a-- <<endl;
+
+cout<< endl;
+
+     int b=7; 
+
+     cout << b++ <<endl;
+     cout << ++b <<endl;
+     cout << --b <<endl;
+     cout << b-- <<endl;
+
+}
