9eb4c13c65bc9d2ec73fb844c327ca0cf8c3564d
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2025-01-03 17:16:37 +0530

    Add files via upload

---
+++ Functions/ncr.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std ;
+
+int factorial(int n){
+    int fact = 1;
+
+    for (int i=1 ; i<=n ; i++){
+        fact = fact * i;
+    }
+    
+    return fact ;
+}
+
+int ncr(int n , int r){
+
+    int numerator = factorial(n);
+    int denominator  = factorial(r) * factorial(n-r);
+    int ans = numerator / denominator ; 
+    
+    return ans;
+
+}
+
+int main (){
+
+int n , r;
+cin >> n>>r;
+
+cout<< "Answer : " << ncr(n,r)<< endl;
+    
+}
